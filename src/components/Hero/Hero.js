5b44e0283d457a873e0e47486a300bf35dcaa52e
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-02-14 11:20:20 -0300

    feat: hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of this website is to provide a look to my skills as web developer.
+      </SectionText>
+      <Button onClick={() => window.location = "https://www.google.com"}>Look more</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
